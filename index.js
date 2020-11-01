6d2b6fca163ea89fcd2ba860df8f12bd03ad579a
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-01 16:41:38 +0100

    added other event listeners

--- index.js
+++ index.js
@@ -17,8 +17,24 @@ for (let input of inputs) {
 }
 
 document.getElementById("bubble").addEventListener("click", () => {
+  sortArray(bubbleSort);
+});
+
+document.getElementById("insert").addEventListener("click", () => {
+  sortArray(insertionSortBetter);
+});
+
+document.getElementById("select").addEventListener("click", () => {
+  sortArray(selectionSort);
+});
+
+document.getElementById("merge").addEventListener("click", () => {
+  sortArray(mergeSort);
+});
+
+function sortArray(callback) {
   for (let i = 0; i < numbers.length; i++) {
-    let result = bubbleSort(numbers[i]);
+    let result = callback(numbers[i]);
     outputs[i].textContent = result.join(", ");
   }
-});
+}
