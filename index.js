a898809a6f5ba93517eeb457d8bc11003fbd694e
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-01 16:27:08 +0100

    getting numbers from html

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ import {
   selectionSort,
   mergeSort,
 } from "./sorting.js";
+
+let inputs = document.querySelectorAll("[class^='numbers']");
+let numbers = [];
+
+for (let input of inputs) {
+  let number = input.textContent.split(",").map((item) => {
+    return parseInt(item);
+  });
+  numbers.push(number);
+}
